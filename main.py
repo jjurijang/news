2380e9a2524cd22e9b21c664573c68799eef313f
jjurijang <165987742+jjurijang@users.noreply.github.com>
2025-02-24 12:51:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 # main.py
 
 from selenium import webdriver as wb
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-import requests
-import os
 import pandas as pdfrom job_expo import scrape_job_expo
 
 if __name__ == "__main__":
